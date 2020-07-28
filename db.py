abc32c043335df45f5a03dadbb57e065540e0ec6
Kirill Inyakin <kirillinyakin@yandex.ru>
2020-07-28 16:46:47 +0300

    fixes

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 import mysql.connector
 import parsing
+import os
 
 def create_connection():
     connection = None
     try :
         connection = mysql.connector.connect(
-            host='eu-cdbr-west-03.cleardb.net',
-            user='b792e6d9b972c5',
-            passwd='5619ff16',
+            host='localhost',
+            user='root',
+            passwd=os.getenv('passwd'),
             database='heroku_61c7527e4590fca'
         )
         connection.autocommit = True
