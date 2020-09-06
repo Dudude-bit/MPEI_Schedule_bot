7fdad8b12ff1227a3b3a9e615a11964de8e78c97
root <root@348105-ct82002.tmweb.ru>
2020-09-06 13:27:07 +0000

    fixes

--- launch_script.sh
+++ launch_script.sh
@@ -1,3 +1,4 @@
 source /root/mpei_bot/mpei_bot_env/bin/activate
 python /root/mpei_bot/MPEI_Schedule_bot/delete_schedule.py
+python /root/mpei_bot/MPEI_Schedule_bot/pass_errors.py
 python /root/mpei_bot/MPEI_Schedule_bot/main.py
