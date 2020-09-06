6b0d10312d68966cb64d1241c4d30415d1f37a5c
root <root@348105-ct82002.tmweb.ru>
2020-09-06 13:17:40 +0000

    fixes

--- launch_sript.sh
+++
@@ -1,7 +0,0 @@
-source /root/mpei_bot/mpei_bot_env/bin/activate;
-echo 'VENV ACTIVATED';
-python /root/mpei_bot/main_dir/MPEI_Schedule_bot/pass_errors.py;
-echo 'PASSED ERRORS'
-python /root/mpei_bot/main_dir/MPEI_Schedule_bot/delete_schedule.py;
-echo 'SCHEDULE DELETED'
-python /root/mpei_bot/main_dir/MPEI_Schedule_bot/main.py;
