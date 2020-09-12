16afebaeca90aa2139b88905e2af50432435b8a0
Dudude <kirillinyakin@yandex.ru>
2020-09-12 18:39:01 +0300

    changed launch_script

--- launch_script.sh
+++ launch_script.sh
@@ -1,3 +1,4 @@
 source /root/mpei_bot/mpei_bot_env/bin/activate
+cd /root/mpei_bot/MPEI_Schedule_bot && git pull
 python /root/mpei_bot/MPEI_Schedule_bot/delete_schedule.py
 python /root/mpei_bot/MPEI_Schedule_bot/main.py
