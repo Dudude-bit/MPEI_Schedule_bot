22e25ce60d24f4fbfa6d890d3e3cb7e5d9993430
Dudude <kirillinyakin@yandex.ru>
2020-10-03 00:26:27 +0300

    added postgres

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE mpei_bot;
+CREATE TABLE schedule (
+  id serial unique,
+  WeekDay VARCHAR(250) NOT NULL,
+  num_object smallint NOT NULL,
+  object varchar(250) NOT NULL,
+  groupoid int NOT NULL,
+  auditory varchar(250) NOT NULL,
+  teacher varchar(250) NOT NULL,
+  object_type varchar(250) NOT NULL,
+  slug varchar(8) NOT NULL,
+  week smallint NOT NULL,
+  PRIMARY KEY (id)
+);
