ff870ea2e4edf86ab33d38887f4fbb6a2298eab8
Dudude <kirillinyakin@yandex.ru>
2020-10-02 22:31:46 +0300

    fixes

--- 123.py
+++ 123.py
@@ -1,5 +1,2 @@
-import yaml
-
-with open('.jobber', 'r') as f:
-    yaml = yaml.safe_load(f)
-print(yaml)
+while True:
+    pass
--- main.py
+++ main.py
@@ -24,6 +24,9 @@ redis = redis.Redis(host='redis')
 
 ALLOWED_BARS_USER_IDS = set(map(int, map(lambda x: x.decode('utf8'), redis.smembers('allowed_users_id'))))
 
+print(ALLOWED_BARS_USER_IDS)
+
+print('here')
 
 @bot.message_handler(commands=['start'])
 @decorator
